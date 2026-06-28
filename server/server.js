8328e962ae6424b7e6d4fb27070ad79aae468088
davidkirubha <kirubhakaranpaul@01gmail.com>
2026-06-28 12:27:04 +0530

    fix MongoDB URI and Render port

--- server/server.js
+++ server/server.js
@@ -1,123 +1,52 @@
-const express=require("express")
+const express = require("express");
 
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const cors=require("cors")
+const cors = require("cors");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const authRoutes=require(
-"./routes/authRoutes"
-)
+const authRoutes = require("./routes/authRoutes");
 
-const aiRoutes=require(
-"./routes/aiRoutes"
-)
+const aiRoutes = require("./routes/aiRoutes");
 
-const workoutRoutes=require(
-"./routes/workoutRoutes"
-)
+const workoutRoutes = require("./routes/workoutRoutes");
 
-const mealRoutes=require(
-"./routes/mealRoutes"
-)
+const mealRoutes = require("./routes/mealRoutes");
 
-const progressRoutes=require(
-"./routes/progressRoutes"
-)
+const progressRoutes = require("./routes/progressRoutes");
 
-const motivationRoutes=require(
-"./routes/motivationRoutes"
-)
+const motivationRoutes = require("./routes/motivationRoutes");
 
-const app=express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect(
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-process.env.MONGO_URL
+app.use("/api/auth", authRoutes);
 
-)
+app.use("/api/ai", aiRoutes);
 
-.then(()=>{
+app.use("/api/workout", workoutRoutes);
 
-console.log(
+app.use("/api/meal", mealRoutes);
 
-"MongoDB Connected"
+app.use("/api/progress", progressRoutes);
 
-)
+app.use("/api/motivation", motivationRoutes);
 
-})
+const PORT = process.env.PORT || 5000;
 
-.catch((err)=>{
-
-console.log(err)
-
-})
-
-app.use(
-
-"/api/auth",
-
-authRoutes
-
-)
-
-app.use(
-
-"/api/ai",
-
-aiRoutes
-
-)
-
-app.use(
-
-"/api/workout",
-
-workoutRoutes
-
-)
-
-app.use(
-
-"/api/meal",
-
-mealRoutes
-
-)
-
-app.use(
-
-"/api/progress",
-
-progressRoutes
-
-)
-
-app.use(
-
-"/api/motivation",
-
-motivationRoutes
-
-)
-
-app.listen(
-
-5000,
-
-()=>{
-
-console.log(
-
-"Server Running"
-
-)
-
-}
-
-)
+app.listen(PORT, () => {
+  console.log("Server Running");
+});
